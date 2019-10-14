6c258c4e0627f26176d5176f8b4a61decdb79877
Carla Manosa <carlamanosa@yahoo.com>
2019-10-14 07:43:30 -0700

    updated new code

---
+++ index.js
@@ -0,0 +1,15 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+
+const questions = [
+  
+];
+
+function writeToFile(fileName, data) {
+ 
+}
+
+function init() {
+
+init();
